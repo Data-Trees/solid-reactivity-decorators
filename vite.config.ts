cc60ae45a5b5598c3918f342b1901d5ddb370ed5
dancherouny <dan@spacio.ai>
2025-03-28 15:14:54 +0100

    Move deps around

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vitest/config';
 import solid from 'vite-plugin-solid';
 import path from 'path';
 import packageJson from './package.json';
 import dts from 'vite-plugin-dts';
+import {defineConfig} from "vite";
 
 export default defineConfig({
   plugins: [
